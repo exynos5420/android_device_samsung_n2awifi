cfa2a2dce8722cc99f283ceb979f4125e27e00d6
deadman96385 <seanhoyt963@gmail.com>
2016-11-11 14:52:59 -0600

    Use Exynos5420-common
    Change-Id: I5d0722cc739e9b8f94d13dc47a2ac3e6e1f20f01

--- cm.mk
+++ cm.mk
@@ -1,4 +1,8 @@
-$(call inherit-product, device/samsung/n2awifi/full_n2awifi.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from n2awifi device
+$(call inherit-product, device/samsung/n2awifi/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
@@ -10,4 +14,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T520 \
     PRODUCT_NAME=n2awifi \
     PRODUCT_DEVICE=n2awifi \
-    TARGET_DEVICE=n2awifi \
+    TARGET_DEVICE=n2awifi
