6562c8e4c145111df25bc33d2bbabee2ae31d780
Thomas Patry <thompatry@gmail.com>
2017-01-05 23:43:51 -0600

    n2awifi:Fixes OK Google.
    I hate this damn bug. I would have never thought to look here.
    
    Basically changes the order of the inherit are called.
    
    Change-Id: I36a0ea4570035d05f415e844d162d54465c8ce8f

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -1,13 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE            := fstab.universal5420
-LOCAL_MODULE_TAGS       := optional eng
-LOCAL_MODULE_CLASS      := ETC
-LOCAL_SRC_FILES         := fstab.universal5420
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE		:= init.target.rc
 LOCAL_MODULE_TAGS	:= optional eng
 LOCAL_MODULE_CLASS	:= ETC
