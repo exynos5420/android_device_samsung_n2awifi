ba3a7de7dbecbb1df1796fa2eae702f55c3ac4f1
Thomas Patry <thompatry@gmail.com>
2017-01-05 23:43:51 -0600

    n2awifi:Fixes OK Google.
    I hate this damn bug. I would have never thought to look here.
    
    Basically changes the order of the inherit are called.
    
    Change-Id: I36a0ea4570035d05f415e844d162d54465c8ce8f

--- cm.mk
+++ cm.mk
@@ -4,6 +4,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n2awifi device
 $(call inherit-product, device/samsung/n2awifi/device.mk)
 
+PRODUCT_NAME := full_n2awifi
+PRODUCT_DEVICE := n2awifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T520
+
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
