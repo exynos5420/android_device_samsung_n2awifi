737969005e18753b0cf462f8da4db8c71f042beb
Thomas Patry <thompatry@gmail.com>
2017-01-06 18:43:18 -0600

    Clean up cm.mk
    Change-Id: I85f032d711f8fd6fcc78afa58c4fe73e1b94b85a

--- cm.mk
+++ cm.mk
@@ -4,11 +4,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n2awifi device
 $(call inherit-product, device/samsung/n2awifi/device.mk)
 
-PRODUCT_NAME := full_n2awifi
-PRODUCT_DEVICE := n2awifi
 PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := SM-T520
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
