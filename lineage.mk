8a48398eb720f413d8a9c25b4d93bf4680fd3928
LaCruz <lacruz@gmx.net>
2017-01-24 18:58:12 +0100

    small changeds to get working cm14.1 version, bas to get working cm14.1 version, based on n1awifi
    Change-Id: I925b943b6ba7ba5ed4508a02c3173a642c15e962

--- lineage.mk
+++ lineage.mk
@@ -4,12 +4,16 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from n2awifi device
 $(call inherit-product, device/samsung/n2awifi/device.mk)
 
+PRODUCT_NAME := full_n2awifi
+PRODUCT_DEVICE := n2awifi
 PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T520
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_n2awifi
+PRODUCT_NAME := lineage_n2awifi
 PRODUCT_DEVICE := n2awifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
