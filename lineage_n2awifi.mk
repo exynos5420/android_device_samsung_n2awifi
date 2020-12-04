836868b3b9ad0f9ab88b404670f06559df8509a0
LaCruz <lacruz@gmx.net>
2020-12-04 10:26:57 +0100

    n2awifi: Consider lineage_n2awifi product via the AOSP way
    Change-Id: I2d5a09d3dc84471f14dca6953cefd3ec49d66870

--- lineage_n2awifi.mk
+++ lineage_n2awifi.mk
@@ -7,7 +7,7 @@ $(call inherit-product, device/samsung/n2awifi/device.mk)
 PRODUCT_BRAND := samsung
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
 
 PRODUCT_NAME := lineage_n2awifi
 PRODUCT_DEVICE := n2awifi
@@ -16,7 +16,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T520 \
     PRODUCT_NAME=n2awifi \
     PRODUCT_DEVICE=n2awifi \
-    PRIVATE_BUILD_DESC="samsung/picassowifiue/picassowifi:4.4.2/KOT49H/T520UEUAOD1:user/release-keys" \
     TARGET_DEVICE=n2awifi
-
-BUILD_FINGERPRINT := samsung/picassowifiue/picassowifi:4.4.2/KOT49H/T520UEUAOD1:user/release-keys
